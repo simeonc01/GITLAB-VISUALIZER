dd5158b91c362e5b9625fd81d65b60a4191d9d2a
unknown <simeonc@stud.ntnu.no>
2022-09-18 15:32:26 +0200

    feat(#13): inputs forms for loading of new project are created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,32 +13,59 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
-
+import Modal from '@mui/material/Modal';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import TextField from "@mui/material/TextField";
 
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window?: () => Window;
 }
 
 const drawerWidth = 240;
-const navItems = ['Prosjekt Navn'];
 
-export default function DrawerAppBar(props: Props) {
+const style = {
+  position: 'absolute' ,
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export default function Header(props: Props) {
+
   const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-  const [showEditor, setShowEditor] = useState(false);
+  const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+
+  const [project, setProject] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const [url, setUrl] = useState<string>("");
+  const [tokenID, setTokenID] = useState<string>("");
+
+  /**
+   * Do a check if there is a project in the local storage.
+   * If so, set the variabel project true. 
+   * If not, let it remain false.
+   */
+
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
-    
   };
 
   const click = () => {
-    console.log("click");
-    setShowEditor(true)
+    setOpen(!open);
+  };
+
+  const clearProject = () => {
+    setProject(false)
+    
   };
 
   const drawer = (
@@ -48,12 +75,20 @@ export default function DrawerAppBar(props: Props) {
       </Typography>
       <Divider />
       <List onClick={click}>
-        <ListItem key={"Prosjekt Navn"} disablePadding >
-          <ListItemButton sx={{ textAlign: 'center'}}>
-            <AddIcon/>
-            <ListItemText primary={"Legg til prosjekt"}/>
-          </ListItemButton>
-        </ListItem>
+        <div>{!project ?
+            <ListItem key={"Legg til prosjekt"} disablePadding >
+            <ListItemButton sx={{ textAlign: 'center'}}>
+              <AddIcon/>
+              <ListItemText primary={"Legg til prosjekt"}/>
+            </ListItemButton>
+          </ListItem> :
+          <ListItem key={"Prosjekt Navn"} disablePadding >
+            <ListItemButton sx={{ textAlign: 'center'}}>
+              <ListItemText primary={"Prosjekt Navn"}/>
+            </ListItemButton>
+          </ListItem> 
+          }
+        </div>
       </List>
     </Box>
   );
@@ -61,55 +96,128 @@ export default function DrawerAppBar(props: Props) {
   const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ ml: 3, flexGrow: 1, display: { xs: 'none', sm: 'block'} }}
-          >
-            GitLab Visualizer
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-            
-            <Button onClick={click} key={"Legg til prosjekt"} sx={{ color: '#fff' }}>
-              <AddIcon sx={{mr: 1}}/>
-              Legg til prosjekt
+    <div>
+      <Modal
+        open={open}
+        onClose={click}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >{project?
+        <Box sx={{ ...style }}>
+          <ArrowBackIcon onClick={click}/>
+          <h2 style={{textAlign: "center"}}>Prosjekt Navn</h2>
+          <p>
+            URL: ...
+          </p>
+          <p>
+            Token id: ...
+          </p>
+          <h5 style={{color:"#0047AB"}} onClick={clearProject}>
+            Nytt prosjekt?
+          </h5>
+        </Box> 
+        :
+        <Box sx={{ ...style }}>
+          <ArrowBackIcon onClick={click}/>
+          <h2 style={{textAlign: "center"}}>Legg til prosjekt</h2>
+          <TextField
+              variant="outlined"
+              margin="normal"
+              type="string"
+              required
+              fullWidth
+              id="url"
+              label="URL"
+              name="url"
+              autoComplete="url"
+              autoFocus
+              value={url}
+              onInput={(event: React.ChangeEvent<HTMLInputElement>) => setUrl(event.target.value) }
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              type="string"
+              required
+              fullWidth
+              name="token_id"
+              label="Token ID"
+              id="token_id"
+              autoComplete="token-id"
+              value={tokenID}
+              onInput={(event: React.ChangeEvent<HTMLInputElement>) => setTokenID(event.target.value) }
+            />
+            <br/><br/>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Hent prosjekt
             </Button>
+            <br/><br/>
+        </Box>
+      }
+      </Modal>
 
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Box component="nav">
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
+
+      <Box sx={{ display: 'flex' }}>
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ ml: 3, flexGrow: 1, display: { xs: 'none', sm: 'block'} }}
+            >
+              GitLab Visualizer
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              
+              <div>{!project ?
+                <Button onClick={click}  sx={{ color: '#fff' }}>
+                  <AddIcon sx={{mr: 1}}/>
+                  Legg til prosjekt
+                </Button> 
+                :
+                <Button onClick={click}  sx={{ color: '#fff' }}>
+                  Prosjekt Navn
+                </Button>
+                }
+              </div>
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <Box component="nav">
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box component="main" >
+          <Toolbar />
+        </Box>
       </Box>
-      <Box component="main" >
-        <Toolbar />
-      </Box>
-    </Box>
+    </div>
   );
 }
