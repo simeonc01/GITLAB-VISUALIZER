8449d3e85e5c60bc3be1741f36feabffd023d627
vwnorris <vwnorris@stud.ntnu.no>
2022-09-22 11:47:58 +0200

    Created table to show data

--- src/components/CommitComponent.tsx
+++ src/components/CommitComponent.tsx
@@ -1,7 +1,19 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Commit } from "../util/types";
 import { GitLabContext } from "./GitlabProvider";
-import GitlabProvider from "./GitlabProvider";
+import Box from "@mui/material/Box";
+import Collapse from "@mui/material/Collapse";
+import IconButton from "@mui/material/IconButton";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export default function CommitComponent() {
   const [commits, setCommits] = useState<Commit[]>([]);
@@ -10,10 +22,132 @@ export default function CommitComponent() {
 
   useEffect(() => {
     const tempCommits = context.commits;
-    if(tempCommits !== null) setCommits(tempCommits);
+    if (tempCommits !== null) setCommits(tempCommits);
     console.log(commits);
-
   }, [context.commits]);
 
-  return <div>CommitComponent</div>;
+  function createData(
+    name: string,
+    calories: string,
+    fat: number,
+    price: number
+  ) {
+    return {
+      name,
+      calories,
+      fat,
+      price,
+      history: [
+        {
+          date: "2020-01-05",
+          customerId: "11091700",
+          amount: 3,
+        },
+        {
+          date: "2020-01-02",
+          customerId: "Anonymous",
+          amount: 1,
+        },
+      ],
+    };
+  }
+
+  /*
+  const oldRows = [
+    createData("bb", 159,  4.0, 3.99),
+    createData('Ice cream sandwich', 237,  4.3, 4.99),
+    createData('Eclair', 262,  6.0, 3.79),
+    createData('Cupcake', 305,  4.3, 2.5),
+    createData('Gingerbread', 356,  3.9, 1.5),
+  ];
+  */
+
+  const rows = commits.map((commit) => {
+    createData(commit.author_name, commit.title, 3.0, 3.3);
+  });
+
+  function Row(props: { row: ReturnType<typeof createData> }) {
+    const { row } = props;
+    const [open, setOpen] = React.useState(false);
+
+    return (
+      <React.Fragment>
+        <TableRow sx={{ "& > *": { borderBottom: "unset" } }}>
+          <TableCell>
+            <IconButton
+              aria-label="expand row"
+              size="small"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+            </IconButton>
+          </TableCell>
+          <TableCell component="th" scope="row">
+            {row.name}
+          </TableCell>
+          <TableCell>{row.calories}</TableCell>
+          <TableCell align="right">{row.fat}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              <Box sx={{ margin: 1 }}>
+                <Typography variant="h6" gutterBottom component="div">
+                  History
+                </Typography>
+                <Table size="small" aria-label="purchases">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Date</TableCell>
+                      <TableCell>Customer</TableCell>
+                      <TableCell align="right">Amount</TableCell>
+                      <TableCell align="right">Total price ($)</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {row.history.map((historyRow) => (
+                      <TableRow key={historyRow.date}>
+                        <TableCell component="th" scope="row">
+                          {historyRow.date}
+                        </TableCell>
+                        <TableCell>{historyRow.customerId}</TableCell>
+                        <TableCell align="right">{historyRow.amount}</TableCell>
+                        <TableCell align="right">
+                          {Math.round(historyRow.amount * row.price * 100) /
+                            100}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Box>
+            </Collapse>
+          </TableCell>
+        </TableRow>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Commit Component</h1>
+      <TableContainer component={Paper}>
+        <Table aria-label="Commits-table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Commit</TableCell>
+              <TableCell>Author</TableCell>
+              <TableCell align="right">Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <Row key={row.author_name} row={row} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
