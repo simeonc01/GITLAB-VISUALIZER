8b46725871c08a20d3299dad1418f300b319f3a3
vwnorris <vwnorris@stud.ntnu.no>
2022-09-26 09:36:37 +0200

    test(#25): Added snapshot test and folder

--- src/tests/IssuesComponent.test.tsx
+++ src/tests/IssuesComponent.test.tsx
@@ -1,1 +1,19 @@
-import { describe, expect, test } from "@jest/globals";
+import { expect, test } from "@jest/globals";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import IssuesComponent from "../components/IssuesComponent";
+
+afterEach(() => {
+  cleanup();
+});
+
+test("Should render Issues component", () => {
+  //render(<IssuesComponent />);
+  expect(true).toBe(true);
+});
+
+test("Matches snapshot", () => {
+  const tree = renderer.create(<IssuesComponent />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
