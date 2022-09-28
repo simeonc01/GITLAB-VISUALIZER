e65ecdcc13651eaf8c1d678d95b6a511debf72bc
unknown <simeonc@stud.ntnu.no>
2022-09-28 18:54:14 +0200

    feat(#15): local storage for the dates are set up

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Dayjs } from 'dayjs';
+import { useEffect } from 'react';
+import dayjs, { Dayjs } from 'dayjs';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
@@ -10,7 +11,26 @@ import { DesktopDateRangePicker } from '@mui/x-date-pickers-pro/DesktopDateRange
 import { DateRange } from '@mui/x-date-pickers-pro/DateRangePicker';
 
 export default function ResponsiveDateRangePicker() {
-  const [value, setValue] = React.useState<DateRange<Dayjs>>([null, null]);
+  const [value, setValue] = React.useState<DateRange<Dayjs>>([dayjs(localStorage.getItem("startDate")), dayjs(localStorage.getItem("endDate"))]);
+
+  
+  useEffect(() => {
+    if (localStorage.getItem("startDate") == "Invalid Date" || localStorage.getItem("endDate") == "Invalid Date" ) {
+      setValue([null, null]);
+    }
+  }, []);
+
+
+  useEffect(() => {
+    const startDate = value[0]?.toDate().toDateString();
+    const endDate = value[1]?.toDate().toDateString()
+
+    if (startDate !== undefined && endDate !== undefined) {
+        localStorage.setItem("startDate", startDate);
+        localStorage.setItem("endDate", endDate);
+    }
+  }, [value]);
+
 
   return (
     <Stack spacing={3}>
@@ -19,6 +39,7 @@ export default function ResponsiveDateRangePicker() {
         localeText={{ start: 'Startdato', end: 'Sluttdato' }}
       >
         <MobileDateRangePicker
+          
           value={value}
           disableFuture={true}
           onChange={(newValue) => {
@@ -33,6 +54,7 @@ export default function ResponsiveDateRangePicker() {
           )}
         />
       </LocalizationProvider>
+      
     </Stack>
   );
 }
