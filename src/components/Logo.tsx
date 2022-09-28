6767c04e05fe60d5616753672deef1d6afae862b
vwnorris <vwnorris@stud.ntnu.no>
2022-09-28 12:04:00 +0200

    style(#30) added scalability to logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,24 +3,24 @@ import React from "react";
 export default function Logo() {
   return (
     <svg width="55" height="55">
+      <line x1="25" y1="25" x2="47" y2="25" stroke="white" stroke-width="2px" />
       <circle
         cx="25"
         cy="25"
-        r="25"
+        r="40%"
         fill="grey"
         stroke="white"
-        stroke-width="2px"
+        stroke-width="4%"
         mask="url(#quarter)"
       />
-      <line x1="25" y1="0" x2="25" y2="25" stroke="white" stroke-width="2px" />
-      <line x1="25" y1="25" x2="50" y2="25" stroke="white" stroke-width="2px" />
+      <line x1="25" y1="3" x2="25" y2="25" stroke="white" stroke-width="2px" />
       <line
         x1="25"
         y1="25"
-        x2="42.5"
-        y2="7.5"
+        x2="41.5"
+        y2="8.5"
         stroke="white"
-        stroke-width="2px"
+        stroke-width="4px"
       />
       <defs>
         <mask id="quarter">
