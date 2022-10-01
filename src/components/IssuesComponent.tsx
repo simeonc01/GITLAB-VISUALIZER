639bc1cbe55fb433467f5681d1108c8183195d5f
vwnorris <vwnorris@stud.ntnu.no>
2022-10-01 13:24:32 +0200

    feat(#31) added functionality for showing graph for when issues were closed

--- src/components/IssuesComponent.tsx
+++ src/components/IssuesComponent.tsx
@@ -12,7 +12,6 @@ import { GitLabContext } from "./GitlabProvider";
 
 function IssuesComponent() {
   const [issues, setIssues] = useState<Issue[]>([]);
-  const [amount, setAmount] = useState<number>(0);
 
   const context = useContext(GitLabContext);
 
@@ -21,6 +20,7 @@ function IssuesComponent() {
     if (tempIssues !== null) setIssues(tempIssues);
     console.log(tempIssues);
   }, [context.issues]);
+
   function createData(
     title: string,
     assignee: string,
@@ -52,7 +52,7 @@ function IssuesComponent() {
     myData.filter((issue) => {
       const thisDate = new Date(issue.created);
 
-      if (+thisDate <= +now) {
+      if (+thisDate < +now) {
         if (issue.closed === null || new Date(issue.closed) > now) {
           active++;
         }
@@ -61,10 +61,24 @@ function IssuesComponent() {
     return active;
   }
 
-  function countClosed() {
+  function countAllClosed() {
     let closed = 0;
     myData.filter((issue) => {
       issue.closed !== null ? (closed += 1) : (closed += 0);
+    });
+    return closed;
+  }
+
+  function countClosed(now: Date) {
+    let closed = 0;
+    myData.filter((issue) => {
+      const thisDate = new Date(issue.created);
+
+      if (+thisDate < +now) {
+        if (issue.closed !== null) {
+          closed++;
+        }
+      }
     });
     return closed;
   }
@@ -164,6 +178,101 @@ function IssuesComponent() {
     },
   ];
 
+  const graphClosedData = [
+    {
+      date: "12.09",
+      issues: countClosed(new Date(2022, 8, 12)),
+    },
+    {
+      date: "13.09",
+      issues: countClosed(new Date(2022, 8, 13)),
+    },
+    {
+      date: "14.09",
+      issues: countClosed(new Date(2022, 8, 14)),
+    },
+    {
+      date: "15.09",
+      issues: countClosed(new Date(2022, 8, 15)),
+    },
+    {
+      date: "16.09",
+      issues: countClosed(new Date(2022, 8, 16)),
+    },
+    {
+      date: "17.09",
+      issues: countClosed(new Date(2022, 8, 17)),
+    },
+    {
+      date: "18.09",
+      issues: countClosed(new Date(2022, 8, 18)),
+    },
+    {
+      date: "19.09",
+      issues: countClosed(new Date(2022, 8, 19)),
+    },
+    {
+      date: "20.09",
+      issues: countClosed(new Date(2022, 8, 20)),
+    },
+    {
+      date: "21.09",
+      issues: countClosed(new Date(2022, 8, 21)),
+    },
+    {
+      date: "22.09",
+      issues: countClosed(new Date(2022, 8, 22)),
+    },
+    {
+      date: "23.09",
+      issues: countClosed(new Date(2022, 8, 23)),
+    },
+    {
+      date: "24.09",
+      issues: countClosed(new Date(2022, 8, 24)),
+    },
+    {
+      date: "25.09",
+      issues: countClosed(new Date(2022, 8, 25)),
+    },
+    {
+      date: "26.09",
+      issues: countClosed(new Date(2022, 8, 26)),
+    },
+    {
+      date: "27.09",
+      issues: countClosed(new Date(2022, 8, 27)),
+    },
+    {
+      date: "28.09",
+      issues: countClosed(new Date(2022, 8, 28)),
+    },
+    {
+      date: "29.09",
+      issues: countClosed(new Date(2022, 8, 29)),
+    },
+    {
+      date: "30.09",
+      issues: countClosed(new Date(2022, 8, 30)),
+    },
+    {
+      date: "01.10",
+      issues: countClosed(new Date(2022, 9, 1)),
+    },
+    {
+      date: "02.10",
+      issues: countClosed(new Date(2022, 9, 2)),
+    },
+    {
+      date: "03.10",
+      issues: countClosed(new Date(2022, 9, 3)),
+    },
+    {
+      date: "04.10",
+      issues: countClosed(new Date(2022, 9, 4)),
+    },
+  ];
+
   return (
     <div style={{ display: "grid", gridTemplateColumns: "500px 150px" }}>
       <div>
@@ -176,7 +285,7 @@ function IssuesComponent() {
             </linearGradient>
           </defs>
           <XAxis dataKey="date" />
-          <YAxis />
+          <YAxis padding={{ top: 10 }} />
           <CartesianGrid strokeDasharray="3 3" />
           <Tooltip />
           <Area
@@ -194,7 +303,7 @@ function IssuesComponent() {
         <p>Total issues:</p>
         <h3>{myData.length}</h3>
         <p>Total closed issues:</p>
-        <h3>{countClosed()}</h3>
+        <h3>{countAllClosed()}</h3>
       </div>
     </div>
   );
